4f1600aa332cc1878f07cc6d4231fcf3692b10e9
evilPaprika <ivil.paprika@gmail.com>
2019-03-31 09:28:03 +0500

    3 задача, надо ее еще порефакторить

--- first_task.py
+++ first_task.py
@@ -35,17 +35,17 @@ class FirstTask(QWidget):
         self.draw_function(qp)
         qp.end()
 
-    def coords_to_screen_point(self, x, y):
+    def cartesian_to_screen(self, x, y):
         return round((x - self.start) * self.geometry().width() / (self.end - self.start)), \
                round((y - self.ymax) * self.geometry().height() / (self.ymin - self.ymax))
 
-    def screen_point_to_coords(self, x, y):
-        return x * (self.end - self.start) / self.geometry().width() + self.start, \
-               y * (self.ymin - self.ymax) / self.geometry().height() + self.ymax
+    def screen_to_cartesian(self, xx, yy):
+        return xx * (self.end - self.start) / self.geometry().width() + self.start, \
+               yy * (self.ymin - self.ymax) / self.geometry().height() + self.ymax
 
     def draw_axes(self, qp):
         qp.setPen(QPen(Qt.black, 1, Qt.SolidLine))
-        xx0, yy0 = self.coords_to_screen_point(0, 0)
+        xx0, yy0 = self.cartesian_to_screen(0, 0)
         qp.drawLine(xx0, 0, xx0, self.geometry().height())
         qp.drawLine(0, yy0, self.geometry().width(), yy0)
         self.draw_axes_notches(qp, xx0, yy0)
@@ -53,19 +53,19 @@ class FirstTask(QWidget):
     def draw_axes_notches(self, qp, xx0, yy0):
         qp.setBrush(QBrush(Qt.SolidPattern))
         qp.setFont(QFont('Decorative', 7))
-        step = max(1, round(self.screen_point_to_coords(32, 0)[0] - self.screen_point_to_coords(0, 0)[0]))
+        step = max(1, round(self.screen_to_cartesian(32, 0)[0] - self.screen_to_cartesian(0, 0)[0]))
         for x in range(step, max(abs(self.start), abs(self.end)), step):
-            xx_right, yy = self.coords_to_screen_point(x, 0)
-            xx_left, yy = self.coords_to_screen_point(-x, 0)
+            xx_right, yy = self.cartesian_to_screen(x, 0)
+            xx_left, yy = self.cartesian_to_screen(-x, 0)
             qp.drawLine(xx_right, yy - 2, xx_right, yy + 2)
             qp.drawLine(xx_left, yy - 2, xx_left, yy + 2)
             qp.drawText(QRect(xx_right - 10, yy + 10, 20, 20), Qt.AlignCenter, str(x))
             qp.drawText(QRect(xx_left - 10, yy + 10, 20, 20), Qt.AlignCenter, str(-x))
 
-        step = max(1, round(self.screen_point_to_coords(0, 0)[1] - self.screen_point_to_coords(0, 32)[1]))
+        step = max(1, round(self.screen_to_cartesian(0, 0)[1] - self.screen_to_cartesian(0, 32)[1]))
         for y in range(step, max(abs(self.ymin), abs(self.ymax)), step):
-            xx, yy_up = self.coords_to_screen_point(0, y)
-            xx, yy_down = self.coords_to_screen_point(0, -y)
+            xx, yy_up = self.cartesian_to_screen(0, y)
+            xx, yy_down = self.cartesian_to_screen(0, -y)
             qp.drawLine(xx - 2, yy_up, xx + 2, yy_up)
             qp.drawLine(xx - 2, yy_down, xx + 2, yy_down)
             qp.drawText(QRect(xx + 10, yy_up - 10, 20, 20), Qt.AlignCenter, str(y))
@@ -80,10 +80,10 @@ class FirstTask(QWidget):
 
     def draw_function(self, qp):
         qp.setPen(QPen(Qt.blue, 1, Qt.SolidLine))
-        prev_point = QPoint(*self.coords_to_screen_point(self.start, self.function(self.start)))
+        prev_point = QPoint(*self.cartesian_to_screen(self.start, self.function(self.start)))
         for xx in range(1, self.geometry().width()):
-            x, _ = self.screen_point_to_coords(xx, 0)
-            _, yy = self.coords_to_screen_point(0, self.function(x))
+            x, _ = self.screen_to_cartesian(xx, 0)
+            _, yy = self.cartesian_to_screen(0, self.function(x))
             new_point = QPoint(xx, yy)
             qp.drawLine(prev_point, new_point)
             prev_point = new_point
--- third_task.py
+++ third_task.py
@@ -4,7 +4,7 @@ from typing import List, Any
 
 from PyQt5.QtWidgets import QWidget, QApplication
 from PyQt5.QtGui import QPainter, QPen, QPolygon, QBrush, QFont, QMouseEvent
-from PyQt5.QtCore import Qt, QPoint, QRect
+from PyQt5.QtCore import Qt, QPoint, QRect, QLine
 import sys
 
 
@@ -19,6 +19,8 @@ class FirstTask(QWidget):
         self.polygon = []
         self.polygon_shards = []
         self.concave_vertices = []
+
+        self.get_line_intersection(QPoint(10, 0), QPoint(10, 1), QPoint(5, 5), QPoint(15, 5))
 
         self.init_ui()
 
@@ -36,41 +38,136 @@ class FirstTask(QWidget):
                 self.update()
                 return
             self.polygon_is_build = True
-            self.split_polygon()
             self.update()
 
     def paintEvent(self, e):
         qp = QPainter()
         qp.begin(self)
-        self.paint_polygon(qp)
+        qp.setPen(QPen(Qt.blue, 3, Qt.SolidLine))
+
+        self.paint_polygon(qp, self.polygon)
         if self.polygon_is_build:
+            self.polygon_shards = self.split_polygon(self.polygon, qp)
+
+            for poly in self.polygon_shards:
+                self.paint_polygon(qp, poly)
             qp.setPen(QPen(Qt.red, 3, Qt.SolidLine))
             for vertex in self.concave_vertices:
                 qp.drawPoint(vertex)
 
         qp.end()
 
-    def split_polygon(self):
-        self.find_concave_vertices()
+    def split_polygon(self, polygon, qp):
+        # self.concave_vertices = self.find_concave_vertices(polygon)
+        concave_vertex = self.find_concave_vertex(polygon)
+        if not concave_vertex:
+            return [polygon]
+        edges = [QLine(*(polygon + polygon)[i:i + 2]) for i in range(0, len(polygon), 1)]
+        print(polygon)
+        self.concave_vertices.append(concave_vertex)
+        i = polygon.index(concave_vertex)
 
-    def find_concave_vertices(self):
-        # TODO добавить точки чтобы был конец
-        for i in range(2, len(self.polygon)):
-            if (self.polygon[i - 1].x() - self.polygon[i - 2].x()) * (self.polygon[i].y() - self.polygon[i - 1].y()) - (
-                    self.polygon[i].x() - self.polygon[i - 1].x()) * (
-                    self.polygon[i - 1].y() - self.polygon[i - 2].y()) < 0:
-                self.concave_vertices.append(self.polygon[i - 1])
+        intersection, split_edge = self.find_closest_intersection(polygon[i - 1], concave_vertex, edges)
 
-    def paint_polygon(self, qp):
+        if not split_edge or not intersection:
+            return [polygon]
+        polygon_one = [intersection]
+        polygon_two = [intersection, concave_vertex]
+        j = 1
+        while True:
+            vertex_one = polygon[i - j]
+            if vertex_one == split_edge.p1():
+                polygon_one.append(vertex_one)
+                polygon_two.insert(0, split_edge.p2())
+                break
+            if vertex_one == split_edge.p2():
+                polygon_one.append(vertex_one)
+                polygon_two.insert(0, split_edge.p1())
+                break
+            j += 1
+            polygon_one.append(vertex_one)
+        k = 1
+        while True:
+            vertex_two = polygon[(i + k) % len(polygon)]
+            if vertex_two == split_edge.p1():
+                break
+            if vertex_two == split_edge.p2():
+                break
+            k += 1
+            polygon_two.append(vertex_two)
+        return self.split_polygon(polygon_one, qp) + (self.split_polygon(polygon_two, qp))
+
+    def find_concave_vertex(self, polygon):
+        if self.area_by_shoelace(polygon) < 0:
+            polygon = polygon[::-1]
+        for i in range(0, len(polygon)):
+            if (polygon[i - 1].x() - polygon[i - 2].x()) * (polygon[i].y() - polygon[i - 1].y()) - (
+                    polygon[i].x() - polygon[i - 1].x()) * (
+                    polygon[i - 1].y() - polygon[i - 2].y()) < 0:
+                return polygon[i - 1]
+
+    def find_concave_vertices(self, polygon):
+        print(self.area_by_shoelace(polygon))
+        if self.area_by_shoelace(polygon) < 0:
+            polygon = polygon[::-1]
+        concave_vertices = []
+        for i in range(0, len(polygon)):
+            if (polygon[i - 1].x() - polygon[i - 2].x()) * (polygon[i].y() - polygon[i - 1].y()) - (
+                    polygon[i].x() - polygon[i - 1].x()) * (
+                    polygon[i - 1].y() - polygon[i - 2].y()) < 0:
+                concave_vertices.append(polygon[i - 1])
+        return concave_vertices
+
+    def find_closest_intersection(self, p0: QPoint, p1: QPoint, edges):
+        min_intersection, split_edge = None, None
+        min_length = None
+        for edge in edges:
+            intersection = self.get_line_intersection(p0, p1, edge.p1(), edge.p2())
+            if intersection and not min_intersection:
+                min_intersection, split_edge = intersection, edge
+                min_length = (p1 - intersection).manhattanLength()
+                continue
+            if intersection and (p1 - intersection).manhattanLength() < min_length:
+                min_intersection, split_edge = intersection, edge
+        return min_intersection, split_edge
+
+    def get_line_intersection(self, p0: QPoint, p1: QPoint, p2: QPoint, p3: QPoint):
+        # https://stackoverflow.com/a/1968345
+        # алгоритм модифицирован чтобы находить пересечение с лучем образованым первой линией
+        s1 = QPoint(p1.x() - p0.x(), p1.y() - p0.y())
+        s2 = QPoint(p3.x() - p2.x(), p3.y() - p2.y())
+
+        try:
+            s = (-s1.y() * (p0.x() - p2.x()) + s1.x() * (p0.y() - p2.y())) / (-s2.x() * s1.y() + s1.x() * s2.y())
+            t = (s2.x() * (p0.y() - p2.y()) - s2.y() * (p0.x() - p2.x())) / (-s2.x() * s1.y() + s1.x() * s2.y())
+
+            if 0 < s < 1 and t > 0:
+                intersection = QPoint(p0.x() + (t * s1.x()), p0.y() + (t * s1.y()))
+                # print(intersection)
+                return intersection
+        except ZeroDivisionError:
+            pass
+
+        # print(None)
+        return None
+
+    def area_by_shoelace(self, polygon):
+        area = 0
+        for i in range(len(polygon)):
+            area += (polygon[i - 1].x() - polygon[i].x()) * (polygon[i - 1].y() + polygon[i].y())
+        return area
+
+    def paint_polygon(self, qp, polygon):
         qp.setPen(QPen(Qt.blue, 3, Qt.SolidLine))
-        for vertex in self.polygon:
+        for vertex in polygon:
             qp.drawPoint(vertex)
+            qp.drawText(vertex, str(vertex.x()) + ", " + str(vertex.y()))
         qp.setPen(QPen(Qt.blue, 1, Qt.SolidLine))
-        for i in range(1, len(self.polygon)):
-            qp.drawLine(self.polygon[i - 1], self.polygon[i])
+        for i in range(1, len(polygon)):
+            qp.drawLine(polygon[i - 1], polygon[i])
 
         if self.polygon_is_build:
-            qp.drawLine(self.polygon[-1], self.polygon[0])
+            qp.drawLine(polygon[-1], polygon[0])
 
 
 if __name__ == '__main__':
