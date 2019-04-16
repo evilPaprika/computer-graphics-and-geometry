448bcdbcff549d58d5639f079dc6b2f5c02c53f3
evilPaprika <ivil.paprika@gmail.com>
2019-04-16 19:31:39 +0500

    чет помнял

--- forth_task.py
+++ forth_task.py
@@ -41,6 +41,8 @@ class ForthTask(QWidget):
         qp.end()
 
     def draw_function(self, qp: QPainter):
+        self.top =[]
+        self.bottom =[]
         for i in range(self.geometry().width()):
             self.top.append(self.geometry().height())
             self.bottom.append(0)
@@ -63,11 +65,11 @@ class ForthTask(QWidget):
                 xx = round((xx - minx) * mx / (maxx - minx))
                 yy = round((yy - miny) * my / (maxy - miny))
                 if (yy > self.bottom[xx]):
-                    qp.setPen(QPen(Qt.blue, 1, Qt.SolidLine))
+                    qp.setPen(QPen(Qt.red, 1, Qt.SolidLine))
                     qp.drawPoint(xx, yy)
                     self.bottom[xx]=yy
                 if (yy < self.top[xx]):
-                    qp.setPen(QPen(Qt.red, 1, Qt.SolidLine))
+                    qp.setPen(QPen(Qt.blue, 1, Qt.SolidLine))
                     qp.drawPoint(xx, yy)
                     self.top[xx]=yy
         self.top =[]
@@ -85,11 +87,11 @@ class ForthTask(QWidget):
                 xx = round((xx - minx) * mx / (maxx - minx))
                 yy = round((yy - miny) * my / (maxy - miny))
                 if (yy > self.bottom[xx]):
-                    qp.setPen(QPen(Qt.blue, 1, Qt.SolidLine))
+                    qp.setPen(QPen(Qt.red, 1, Qt.SolidLine))
                     qp.drawPoint(xx, yy)
                     self.bottom[xx]=yy
                 if (yy < self.top[xx]):
-                    qp.setPen(QPen(Qt.red, 1, Qt.SolidLine))
+                    qp.setPen(QPen(Qt.blue, 1, Qt.SolidLine))
                     qp.drawPoint(xx, yy)
                     self.top[xx]=yy
 
@@ -98,6 +100,9 @@ class ForthTask(QWidget):
 
     def isometric_projection(self, x,y,z):
         return (y-x)*math.sqrt(3.0)/2, (x+y)/2-z
+
+    def dimetric_projection(self, x,y,z):
+        return -x/(2*math.sqrt(2)) + y, x/(2*math.sqrt(2)) - z
 
 def my_exception_hook(exctype, value, traceback):
     print(exctype, value, traceback)
